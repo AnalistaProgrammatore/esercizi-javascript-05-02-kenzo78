5f004104caf591b1ae98ffaf47907960ed206581
kenzo78 <chrispal@hotmail.it>
2020-02-06 22:37:33 +0100

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,3 +1,45 @@
 /**
 * Write a loop that makes seven calls to console.log to output the following triangle
 */
+
+//##########################################################Triangolo Rettangolo######################################################################
+console.log('\n')
+var pezzo = '';
+
+for (var i = 0; i < 7; i++) {
+    pezzo = pezzo + '*';
+    console.log(pezzo);
+}
+
+console.log('_________________________________________________________________' + '\n\n')
+
+//##########################################################Triangolo Isoscile alla stupida :D #######################################################
+
+var contatore = [0, 1, 2, 3, 4, 5, 6, 7]
+for (var i = 0; i <= contatore.length; i++) {
+    if (contatore[i] == 0) {
+        console.log("       *");
+    }
+    if (contatore[i] == 1) {
+        console.log("      ***");
+    }
+    if (contatore[i] == 2) {
+        console.log("     *****");
+    }
+    if (contatore[i] == 3) {
+        console.log("    *******");
+    }
+    if (contatore[i] == 4) {
+        console.log("   *********");
+    }
+    if (contatore[i] == 5) {
+        console.log("  ***********");
+    }
+    if (contatore[i] == 6) {
+        console.log(" *************");
+    }
+    if (contatore[i] == 7) {
+        console.log("***************");
+    }
+
+}
