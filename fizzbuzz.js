b53bc700fd1935becb1dd56081f5bfdefdaeddaa
kenzo78 <chrispal@hotmail.it>
2020-02-07 15:46:53 +0100

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,3 +4,21 @@
 * When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5
 * (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 */
+for (var i = 1; i < 100; i++) {
+    if (i % 15 === 0) console.log("AperolSpritz!");// i numeri divisibili sia per 3 che per 5 sono divisibili per 15, quindi per trovare quei numeri basta usare %15===0
+    else if (i % 3 === 0) console.log("Aperol!");
+    else if (i % 5 === 0) console.log("Spritz!");
+    else console.log(i);
+}
+
+
+console.log(i)
+console.log('____________________________________________________________________________________________________________________________________' + '\n')
+//########################################################### Sviluppo 2 ###############################################################################
+
+messaggio = "";
+for (var i = 1; i <= 100; i++ , messaggio = "") {
+    if (i % 3 === 0) messaggio += "Campari";
+    if (i % 5 === 0) messaggio += "Spritz";
+    console.log(messaggio || i) // in questo modo scrive ciò che è contenuto nella stringa messaggio o il numero, se i numeri sono sia divisibile per 3 che per 5 unisce i messaggi
+}
